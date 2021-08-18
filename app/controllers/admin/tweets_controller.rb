3391db5a8164114cc9febca1a29b15124ba5bbc4
CEmi6653 <86937544+CEmi6653@users.noreply.github.com>
2021-08-18 13:30:47 +0900

    投稿保存機能完了

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,18 +1,37 @@
 class Admin::TweetsController < ApplicationController
+  before_action :authenticate_user!
   before_action :if_not_admin
   before_action :set_tweet, only: [:show, :edit, :destroy]
-
+  
+  def index
+    @tweets = Tweet.all
+  end
+  
   def new
     @tweet = Tweet.new
   end
 
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
   def if_not_admin
-    redirect_to root_path unless current_user.admin?
-      
+      redirect_to root_path unless current_user.admin?
   end
 
   def set_tweet
-    @tweet = Tweet.find(paeams[:id])
+    @tweet = Tweet.find(params[:id])
   end
+
+  def tweet_params
+    params.require(:tweet).permit(:image, :golf_course_name, :postal_code, :bith_place_id, :municipalities, :address, :golf_course_number, :business_hours, :play_environment_id, :golf_course_difficult_id, :average_amount_id, :travel_time_id, :explanation, :cart_information_id).merge(user_id: current_user.id)
+  end
+
+
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,17 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :admin_check, only: [:new, :create]
+
+
   def index
     @tweets = Tweet.all
+  end
+
+  private
+  def admin_check
+    unless current_user.admin?
+      redirect_to root_path
+    end
   end
 
 end
