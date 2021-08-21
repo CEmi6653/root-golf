49b1c02398ca45756175014e78befc5970f114a3
CEmi6653 <86937544+CEmi6653@users.noreply.github.com>
2021-08-21 22:52:00 +0900

    reviewモデルの単体テスト

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = FactoryBot.create(:user)
+    tweet = FactoryBot.create(:tweet)
+    @review = FactoryBot.build(:review, user_id: user.id, tweet_id: tweet.id)
+  end
+
+  context 'コメントの保存ができる時' do
+    it "content,user_id,tweet_idがあると保存できる" do
+      expect(@review).to be_valid
+    end
+  end
+
+  context 'コメントが保存できない場合' do
+    it "contentが空の場合保存できない" do
+      @review.content = ''
+      @review.valid?
+      expect(@review.errors.full_messages).to include("Content can't be blank")
+    end
+  end
+
 end
