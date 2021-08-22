5a4ba67ba124a35c7a2db6cb9024c5efb58d6536
CEmi6653 <86937544+CEmi6653@users.noreply.github.com>
2021-08-22 22:19:52 +0900

    マイページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "tweets#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:show, :edit, :update]
   resources :tweets, only: [:index, :show] do
     resources :reviews, only: [:create, :destroy]
     post 'add' => 'favorites#create'
