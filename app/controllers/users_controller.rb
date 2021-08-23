b32d72862d2727b6968d44665dc3423f08638cf7
CEmi6653 <86937544+CEmi6653@users.noreply.github.com>
2021-08-23 19:50:25 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @tweet = @user.tweets
+    @tweet = @user.tweets.order(created_at: "DESC")
     @nickname = @user.nickname
     redirect_to root_path unless current_user.id == @user.id
   end
