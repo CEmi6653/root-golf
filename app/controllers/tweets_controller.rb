95ac8111ba313131e42a8366a5720c154982fb2b
CEmi6653 <86937544+CEmi6653@users.noreply.github.com>
2021-08-23 17:28:36 +0900

    Revert "frist"
    This reverts commit 9626b2a21db0389215bd33e96464d520623115da.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,10 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :admin_check, only: [:new, :create, :edit, :update, :destroy]
-  before_action :set_q, only: [:index, :search]
+
 
   def index
     @tweets = Tweet.all
-    set_tweet_column 
-    
   end
 
   def show
@@ -16,7 +14,7 @@ class TweetsController < ApplicationController
   end
 
   def search
-    @results = @q.result.includes(:golf_course_name) 
+    @tweets = Tweet.search(params[:keyword])
   end
 
   private
@@ -25,14 +23,5 @@ class TweetsController < ApplicationController
       redirect_to root_path
     end
   end
-
-  def set_q
-    @q = Tweet.ransack(params[:q])  # 検索オブジェクトを生成
-  end
-
-  def set_tweet_column
-    @tweet_name = Tweet.select("golf_course_name").distinct  # 重複なくnameカラムのデータを取り出す
-  end
- 
 
 end
