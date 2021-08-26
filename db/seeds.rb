bd106e9b4e352e6565325d8e1eb31156d02682fb
CEmi6653 <86937544+CEmi6653@users.noreply.github.com>
2021-08-26 20:10:26 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
 User.create!(
-  [
-    {
       nickname:  "管理者",
       email: "admin@example.jp",
       password:  "1111aaaa",
@@ -8,20 +6,6 @@ User.create!(
       bith_place_id: 2,
       play_style_id: 2,
       play_histry_id: 2,
-      admin: true
-    },
+      admin: true)
 
-    
-
-    {
-      nuckname: "39岡崎",
-      email: "test@test.com",
-      password:  "123qwe",
-      password_confirmation: "123qwe",
-      bith_place_id: 15,
-      play_style_id: 2,
-      play_histry_id: 2
-    }
-  ]
-)
     
