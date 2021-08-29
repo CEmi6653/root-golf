a738499ec149a66e02b8c50c52ec707ad268cc8b
CEmi6653 <86937544+CEmi6653@users.noreply.github.com>
2021-08-29 14:34:14 +0900

    修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,6 @@ require("../golfs")
 require("chartkick") 
 require("chart.js")
 require('../preview')
-require("Chart.bundle")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
